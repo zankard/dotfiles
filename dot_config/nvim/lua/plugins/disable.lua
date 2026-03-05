28e1353c56c6a0ee85c2055151c6ade2dbb8d07d
zankard <zankard@gmail.com>
2026-03-05 19:34:07 +0800

    Disable bacon-check.nvim plugin

--- dot_config/nvim/lua/plugins/disable.lua
+++ dot_config/nvim/lua/plugins/disable.lua
@@ -2,4 +2,5 @@ return {
   { "folke/noice.nvim", enabled = false },
   { "markdown-preview.nvim", enabled = false },
   { "olimorris/codecompanion.nvim", enabled = true },
+  { "bacon-check.nvim", enabled = false },
 }
